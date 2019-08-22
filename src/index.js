9f279f599e61bb6eb2cd4271725582ec0ad55afd
Alex Ispa-Cowan <ispacowanguitar@gmail.com>
2019-08-21 20:05:12 -0700

    Build a functional component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-class HelloWorldComponent extends React.Component {
-  render() {
-    return <div>Hello World Component!</div>;
-  }
-}
+const HelloWorldComponent = () => {
+  return <div>Hello World Function Component</div>;
+};
 
 ReactDOM.render(<HelloWorldComponent />, document.getElementById("root"));
